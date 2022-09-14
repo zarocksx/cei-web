7e3050e11ab6c00b35760c0da09082486a042e14
Bryan <bryan.bartoloni@on-hertz.com>
2022-09-14 10:28:43 +0200

    try add static address

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/cei-web/',
+  base: 'https://github.com/zarocksx/cei-web/',
   build: {
     outDir: 'build',
     minify: false,
