2c6fc258063dad867856ac6391968fb27690d320
Bryan <bryan.bartoloni@on-hertz.com>
2022-09-20 14:25:36 +0200

    deploy

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,8 @@
 # abort on errors
 # set -e
 
+git checkout -b deploy
+
 # build
 npm run build
 
@@ -12,14 +14,13 @@ cd build
 # if you are deploying to a custom domain
 echo 'cei.com' > CNAME
 
-git checkout -b deploy
 git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
 git push -f https://github.com/zarocksx/cei-web.git deploy
 git checkout -b main
-read
+
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
 
