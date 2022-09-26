3752512c4e4df6f1661d68f5605335be3714a0c6
Bryan <bryan.bartoloni@on-hertz.com>
2022-09-26 16:09:50 +0200

    added historique timeline

--- src/ts/historique.ts
+++ src/ts/historique.ts
@@ -1,1 +1,7 @@
-console.log('historique')
+import { CEIHeader } from '../components/CEIHeader';
+import '../style/historique.scss';
+
+//import all lit components
+export * from '../components/CEIHeader';
+export * from '../components/CEIFooter'
+export * from '../components/CEICard';
