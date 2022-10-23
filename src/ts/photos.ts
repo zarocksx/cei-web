11131dc27805d66ec7a27f1cee242bcdd56158ee
Bryan <bryan.bartoloni@on-hertz.com>
2022-10-23 14:55:38 +0200

    test url google drive

--- src/ts/photos.ts
+++ src/ts/photos.ts
@@ -27,58 +27,51 @@ header.links = [
 
 mosaic.images = [
   {
-    alt: 'dummy',
-    url: 'https://via.placeholder.com/550',
+    alt: 'old picture',
+    url: 'https://drive.google.com/uc?export=view&id=1rg9_EOrTzEHzB9GakAHMS4FNxOqf6ZyV',
   },
   {
-    alt: 'dummy',
-    url: 'https://via.placeholder.com/750',
+    alt: 'les bleus devant la colonne',
+    url: 'https://drive.google.com/uc?export=view&id=1mVeumyFS0fJiHWujZIjQKPJhs_gs5DEB',
     isWide: true
   },
   {
-    alt: 'dummy',
-    url: 'https://via.placeholder.com/550',
+    alt: 'stream caritatif',
+    url: 'https://drive.google.com/uc?export=view&id=1wGvdbA31TxtU6SH9OkKeWadvTllNyPJb',
   },
   {
-    alt: 'dummy',
-    url: 'https://via.placeholder.com/550',
+    alt: 'ST-V 1989',
+    url: 'https://drive.google.com/uc?export=view&id=1POkvGfkFK-Y52YhNtIxTVa6vA-84eGUU',
     isWide: true
   },
   {
-    alt: 'dummy',
-    url: 'https://via.placeholder.com/550',
+    alt: 'ST-V 2011',
+    url: 'https://drive.google.com/uc?export=view&id=1mjo5ckIYDjgf7B-zgS_r16qK8shgRu0z',
   },
   {
-    alt: 'dummy',
-    url: 'https://via.placeholder.com/550',
-  },
-  {
-    alt: 'dummy',
-    url: 'https://via.placeholder.com/550',
-  },
-  {
-    alt: 'dummy',
-    url: 'https://via.placeholder.com/550',
-  },
-  {
-    alt: 'dummy',
-    url: 'https://via.placeholder.com/550',
-  },
-  {
-    alt: 'dummy',
-    url: 'https://via.placeholder.com/550',
-  },
-  {
-    alt: 'dummy',
-    url: 'https://via.placeholder.com/550',
-  },
-  {
-    alt: 'dummy',
-    url: 'https://via.placeholder.com/550',
-  },
-  {
-    alt: 'dummy',
-    url: 'https://via.placeholder.com/550',
+    alt: 'ST-V 1990',
+    url: 'https://drive.google.com/uc?export=view&id=1GKdiCGcAowd_x-rr5jnH-05KZc8aMn8Y',
     isWide: true
+  },
+  {
+    alt: 'affiche de bal',
+    url: 'https://drive.google.com/uc?export=view&id=1jNhiEPcVfIeWRr4I0dggT-uzz4Gt8DGz',
+    isWide: true
+  },
+  {
+    alt: 'groupe de CEI 1',
+    url: 'https://drive.google.com/uc?export=view&id=168ubjcWzHoZzS3ICbFMO9_USdBhbIaX8',
+  },
+  {
+    alt: 'groupe de CEI 2',
+    url: 'https://drive.google.com/uc?export=view&id=13RoD99O6I59d1E3TXrPkYGoSPvOXecTc',
+  },
+  {
+    alt: 'fondateurs',
+    url: 'https://drive.google.com/uc?export=view&id=1V9eYzsYg1-Z94jSIQtOBPjGlxenci3fa',
+  },
+  {
+    alt: 'groupe de CEI 3',
+    url: 'https://drive.google.com/uc?export=view&id=13wvQbmHQNsG6PVzDv_bfMljovlk-DCLo',
   },
 ]
